e7ce0be38de8da0d86f10a7c51ad39d1dd354e7c
akadashiso <akadashiso@gmail.com>
2021-06-21 17:23:15 +0900

    [Finish]Section8-109 : beforeRouteEnter +

--- bookapp/src/pages/BookEdit.vue
+++ bookapp/src/pages/BookEdit.vue
@@ -1,18 +1,32 @@
 <template>
-  <div>BookEdit
+  <div>
+    BookEdit
+    {{ books.title }}
     {{ books }}
   </div>
 </template>
 
 <script>
 export default {
-  name: 'bookEdit',
-  props:{
-    books: Array
-  }
-}
+  name: "bookEdit",
+  props: {
+    books: Array,
+  },
+  data() {
+    return {
+      book: "",
+    };
+  },
+  beforeRouteEnter(to, from, next) {
+    next((vm) => {
+      vm.$nextTick(() => {
+        vm.book = vm.books[vm.$route.params.id];
+        console.log(vm.book);
+      });
+    });
+  },
+};
 </script>
 
 <style>
-
 </style>
