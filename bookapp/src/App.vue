8b9e5017d844febc2fefd33c202e31366edba577
akadashiso <akadashiso@gmail.com>
2021-06-21 16:56:38 +0900

    [Finish]Section8-107 : App.vueからlocalStorageへ保存

--- bookapp/src/App.vue
+++ bookapp/src/App.vue
@@ -2,7 +2,9 @@
   <v-app>
     <Header />
     <v-main>
-      <router-view />
+      <v-container>
+        <router-view @add-book-list="addBook" />
+      </v-container>
     </v-main>
     <Footer />
   </v-app>
@@ -11,6 +13,8 @@
 <script>
 import Header from "@/global/Header";
 import Footer from "@/global/Footer";
+const STORAGE_KEY = "books";
+
 export default {
   name: "App",
 
@@ -19,8 +23,44 @@ export default {
     Footer,
   },
 
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      books: [],
+      newBook: null,
+    };
+  },
+  mounted() {
+    if (localStorage.getItem(STORAGE_KEY)) {
+      try {
+        this.books = JSON.parse(localStorage.getItem(STORAGE_KEY));
+      } catch (e) {
+        localStorage.removeItem(STORAGE_KEY);
+      }
+    }
+  },
+  methods: {
+    addBook(e) {
+      // 実際に何かしたことを入力する
+
+      this.books.push({
+        id: this.books.length,
+        title: e.title,
+        image: e.image,
+        description: e.description,
+        readDate: "",
+        memo: "",
+      });
+      // this.newBook = "";
+      this.saveBooks();
+    },
+    removeBook(x) {
+      this.books.splice(x, 1);
+      this.saveBooks();
+    },
+    saveBooks() {
+      const parsed = JSON.stringify(this.books);
+      localStorage.setItem(STORAGE_KEY, parsed);
+    },
+  },
 };
 </script>
